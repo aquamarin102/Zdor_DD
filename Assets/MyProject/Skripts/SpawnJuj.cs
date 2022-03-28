2b5eae00903eba949aa1f0d1be2a9e9112fc6ee3
aquamarin102 <97702538+aquamarin102@users.noreply.github.com>
2022-03-28 14:02:51 +0300

    Homework2

---
+++ Assets/MyProject/Skripts/SpawnJuj.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+
+public class SpawnJuj : MonoBehaviour
+{
+    [SerializeField] private float _spawnTime;
+    [SerializeField] private GameObject _prefab;
+
+    private Vector3 _spawnPosition;
+
+    void Start()
+    {
+        
+        StartCoroutine(Spawner());
+        
+    }
+
+    private IEnumerator Spawner()
+    {
+        _spawnPosition = new Vector3(Random.Range(-3f, 3f), 0, Random.Range(-3f, 3f));
+        _prefab.transform.position = _spawnPosition;
+        Instantiate(_prefab);
+        yield return new WaitForSeconds(_spawnTime);
+        StartCoroutine(Spawner());
+    }
+}
